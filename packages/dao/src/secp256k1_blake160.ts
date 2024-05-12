51709596bcef3b89de029a7093be60c434d479f9
phroi <90913182+phroi@users.noreply.github.com>
2024-05-12 13:01:03 +0000

    Remove unnecessary utils

--- packages/dao/src/secp256k1_blake160.ts
+++ packages/dao/src/secp256k1_blake160.ts
@@ -1,13 +1,11 @@
-import { type TransactionSkeletonType, TransactionSkeleton, sealTransaction } from "@ckb-lumos/helpers";
-import { I8Cell, I8Script, witness } from "./cell.js";
+import { type TransactionSkeletonType, sealTransaction } from "@ckb-lumos/helpers";
+import { I8Script, witness } from "./cell.js";
 import { encodeToAddress } from "@ckb-lumos/helpers";
 import { key } from "@ckb-lumos/hd";
 import { defaultScript } from "./config.js";
-import { capacitySifter, hex, lockExpanderFrom } from "./utils.js";
+import { lockExpanderFrom } from "./utils.js";
 import { prepareSigningEntries } from "@ckb-lumos/common-scripts/lib/secp256k1_blake160.js";
-import { addCells, addWitnessPlaceholder } from "./transaction.js";
-import { getCells, getFeeRate, sendTransaction } from "./rpc.js";
-import { ckbFundAdapter, fund } from "./fund.js";
+import { addWitnessPlaceholder } from "./transaction.js";
 
 export function secp256k1Blake160(privateKey: string) {
     const publicKey = key.privateToPublic(privateKey);
@@ -34,46 +32,8 @@ export function secp256k1Blake160(privateKey: string) {
         return sealTransaction(tx, [sig]);
     }
 
-    async function transfer(
-        to: I8Script,
-        ckbAmount: bigint,
-        feeRate?: bigint,
-        secondsTimeout: number = 600 // non-positive number means do not await for transaction to be committed
-    ) {
-        const capacities = await getCapacities();
-
-        const cell = I8Cell.from({
-            capacity: hex(ckbAmount),
-            lock: to,
-        });
-
-        let tx = TransactionSkeleton();
-        tx = addCells(tx, "append", [], [cell]);
-        tx = fund(tx, ckbFundAdapter(lockScript, feeRate ?? await getFeeRate(), preSigner, capacities));
-        return sendTransaction(signer(tx), secondsTimeout);
-    }
-
-    async function getCapacities() {
-        const { capacities } = capacitySifter(
-            await getCells({
-                script: lockScript,
-                scriptType: "lock",
-                filter: {
-                    scriptLenRange: ["0x0", "0x1"],
-                    outputDataLenRange: ["0x0", "0x1"],
-                },
-                scriptSearchMode: "exact"
-            }),
-            expander
-        );
-        return capacities;
-    }
-
     return {
         publicKey, lockScript, address,
-        expander, preSigner, signer,
-        transfer, getCapacities
+        expander, preSigner, signer
     };
 }
-
-export const genesisDevnetKey = "0xd00c06bfd800d27397002dca6fb0993d5ba6399b4238b2f29ee9deb97593d2bc";
