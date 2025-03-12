98cff9daec9ef62c1a99bbef3b46d5732f0302e8
phroi <90913182+phroi@users.noreply.github.com>
2025-03-13 00:21:45 +0700

    Switch to entity subclasses and add validation

--- packages/order/src/codec.ts
+++ packages/order/src/codec.ts
@@ -1,55 +1,141 @@
-import { ccc, mol } from "@ckb-ccc/core";
-import { Int32, UdtData } from "@ickb/dao";
+import { ccc, mol, OutPoint } from "@ckb-ccc/core";
+import { Int32, union } from "@ickb/dao";
 
-export const Ratio = mol.struct({
-  ckbMultiplier: mol.Uint64,
-  udtMultiplier: mol.Uint64,
-});
+export interface RatioLike {
+  ckbScale: ccc.NumLike;
+  udtScale: ccc.NumLike;
+}
 
-export const OrderInfo = mol.struct({
-  ckbToUdt: Ratio,
-  udtToCkb: Ratio,
-  ckbMinMatchLog: mol.Uint8,
-});
+@mol.codec(
+  mol.struct({
+    ckbScale: mol.Uint64,
+    udtScale: mol.Uint64,
+  }),
+)
+export class Ratio extends mol.Entity.Base<RatioLike, Ratio>() {
+  constructor(
+    public ckbScale: ccc.Num,
+    public udtScale: ccc.Num,
+  ) {
+    super();
+  }
 
-export const MintOrderData = mol.struct({
-  padding: mol.Byte32,
-  masterDistance: Int32,
-  orderInfo: OrderInfo,
-});
+  static override from(ratio: RatioLike): Ratio {
+    if (ratio instanceof Ratio) {
+      return ratio;
+    }
 
-export const MatchOrderData = mol.struct({
-  masterOutpoint: ccc.OutPoint,
-  orderInfo: OrderInfo,
-});
+    const { ckbScale, udtScale } = ratio;
+    return new Ratio(ccc.numFrom(ckbScale), ccc.numFrom(udtScale));
+  }
 
-export const PartialOrderData = mol.union({
-  MintOrderData,
-  MatchOrderData,
-});
+  isValid(): boolean {
+    return this.isEmpty() || this.isPopulated();
+  }
 
-export type EncodableOrder = mol.EncodableType<typeof UdtData> &
-  mol.EncodableType<typeof PartialOrderData>;
-export type DecodedOrder = mol.DecodedType<typeof UdtData> &
-  mol.DecodedType<typeof PartialOrderData>;
+  isEmpty(): boolean {
+    return this.ckbScale === 0n && this.udtScale === 0n;
+  }
 
-export const OrderData = mol.Codec.from<DecodedOrder, EncodableOrder>({
-  encode: (encodableOrder) => {
-    return ccc.bytesConcat(
-      UdtData.encode(encodableOrder),
-      PartialOrderData.encode(encodableOrder),
+  isPopulated(): boolean {
+    return this.ckbScale > 0n && this.udtScale > 0n;
+  }
+}
+
+export interface InfoLike {
+  ckbToUdt: RatioLike;
+  udtToCkb: RatioLike;
+  ckbMinMatchLog: ccc.NumLike;
+}
+
+@mol.codec(
+  mol.struct({
+    ckbToUdt: Ratio,
+    udtToCkb: Ratio,
+    ckbMinMatchLog: mol.Uint8,
+  }),
+)
+export class Info extends mol.Entity.Base<InfoLike, Info>() {
+  constructor(
+    public ckbToUdt: Ratio,
+    public udtToCkb: Ratio,
+    public ckbMinMatchLog: number,
+  ) {
+    super();
+  }
+
+  static override from(info: InfoLike): Info {
+    if (info instanceof Info) {
+      return info;
+    }
+
+    const { ckbToUdt, udtToCkb, ckbMinMatchLog } = info;
+    return new Info(
+      Ratio.from(ckbToUdt),
+      Ratio.from(udtToCkb),
+      Number(ckbMinMatchLog),
     );
-  },
-  decode: (bufferLike): DecodedOrder => {
-    const buffer = ccc.bytesFrom(bufferLike);
-    const encodedUdtData = buffer.slice(0, UdtData.byteLength);
-    const encodedOrderData = buffer.slice(UdtData.byteLength);
+  }
 
-    const udtData = UdtData.decode(encodedUdtData);
-    const orderData = PartialOrderData.decode(encodedOrderData);
+  isValid(): boolean {
+    if (this.ckbMinMatchLog < 0 || this.ckbMinMatchLog > 64) {
+      return false;
+    }
 
-    return { ...udtData, ...orderData };
-  },
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  byteLength: UdtData.byteLength! + MintOrderData.byteLength!,
+    if (this.ckbToUdt.isEmpty()) {
+      return this.udtToCkb.isPopulated();
+    }
+
+    if (this.udtToCkb.isEmpty()) {
+      return this.ckbToUdt.isPopulated();
+    }
+
+    return this.ckbToUdt.isPopulated() && this.udtToCkb.isPopulated();
+  }
+}
+
+export interface RelativeLike {
+  padding: ccc.BytesLike;
+  distance: ccc.NumLike;
+}
+
+@mol.codec(
+  mol.struct({
+    padding: mol.Byte32,
+    distance: Int32,
+  }),
+)
+export class Relative extends mol.Entity.Base<RelativeLike, Relative>() {
+  constructor(
+    public padding: ccc.Bytes,
+    public distance: ccc.Num,
+  ) {
+    super();
+  }
+
+  static override from(relative: RelativeLike): Relative {
+    if (relative instanceof Relative) {
+      return relative;
+    }
+
+    const { padding, distance } = relative;
+    return new Relative(ccc.bytesFrom(padding), ccc.numFrom(distance));
+  }
+
+  static getPadding(): ccc.Bytes {
+    return new Uint8Array(32);
+  }
+
+  isValid(): boolean {
+    return !this.padding.some((x) => x !== 0);
+  }
+}
+
+export const DataCodec = mol.struct({
+  udtAmount: mol.Uint128,
+  master: union({
+    relative: Relative,
+    absolute: OutPoint,
+  }),
+  info: Info,
 });
