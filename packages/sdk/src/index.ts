9444274729ab3ced8b93c578219b284eaf03b6a6
phroi <90913182+phroi@users.noreply.github.com>
2025-05-01 03:31:01 +0700

    Solve most todo %35

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,6 +1,6 @@
 import { ccc, type Epoch } from "@ckb-ccc/core";
 import {
-  arrayFrom,
+  collect,
   epochCompare,
   CapacityManager,
   SmartTransaction,
@@ -8,34 +8,37 @@ import {
   type UdtCell,
   type CapacityCell,
   epochAdd,
-  type SmartTransactionLike,
+  asyncBinarySearch,
+  RestrictedTransaction,
 } from "@ickb/utils";
 import {
   convert,
   ICKB_DEPOSIT_CAP,
   ickbExchangeRatio,
-  IckbManager,
+  IckbUdtManager,
   LogicManager,
   OwnedOwnerManager,
   type IckbDepositCell,
   type ReceiptCell,
   type WithdrawalGroups,
 } from "@ickb/core";
-import { DaoManager } from "@ickb/dao";
+import { DaoManager, type DaoCell } from "@ickb/dao";
 import {
   Info,
   OrderManager,
   Ratio,
   type OrderCell,
   type OrderGroup,
+  type RatioLike,
 } from "@ickb/order";
 
 export class IckbController {
   constructor(
-    public ickbManager: IckbManager,
-    public logicManager: LogicManager,
-    public ownedOwnerManager: OwnedOwnerManager,
-    public orderManager: OrderManager,
+    public readonly ickbUdtManager: IckbUdtManager,
+    public readonly logicManager: LogicManager,
+    public readonly ownedOwnerManager: OwnedOwnerManager,
+    public readonly orderManager: OrderManager,
+    public readonly daoManager: DaoManager,
   ) {}
 
   static fromDeps({
@@ -65,37 +68,30 @@ export class IckbController {
       ),
       cellDeps,
     };
-    const ickbManager = IckbManager.fromDeps(ickbXudt, daoManager);
+    const ickbUdtManager = IckbUdtManager.fromDeps(ickbXudt, daoManager);
 
     return new IckbController(
-      ickbManager,
-      LogicManager.fromDeps(ickbLogic, daoManager, ickbManager),
-      OwnedOwnerManager.fromDeps(ownedOwner, daoManager, ickbManager),
-      OrderManager.fromDeps(order, ickbManager),
+      ickbUdtManager,
+      LogicManager.fromDeps(ickbLogic, daoManager, ickbUdtManager),
+      OwnedOwnerManager.fromDeps(ownedOwner, daoManager, ickbUdtManager),
+      OrderManager.fromDeps(order, ickbUdtManager),
+      daoManager,
     );
   }
 
   previewConversion(
     isCkb2Udt: boolean,
     amount: ccc.FixedPoint,
-    options: ({ ratio: Ratio } | { tip: ccc.ClientBlockHeader }) & {
-      fee?: {
-        numerator: ccc.Num;
-        denominator: ccc.Num;
-      };
+    rate: RatioLike | ccc.ClientBlockHeader,
+    fee?: {
+      numerator: ccc.Num;
+      denominator: ccc.Num;
     },
   ): ccc.FixedPoint {
-    // TODO: make compatible with logic.convert(isCkb2Udt, amount, options)!!!
+    let convertedAmount = convert(isCkb2Udt, amount, rate);
 
-    const { ckbScale, udtScale } =
-      "ratio" in options ? options.ratio : ickbExchangeRatio(options.tip);
-
-    let convertedAmount = isCkb2Udt
-      ? (amount * ckbScale) / udtScale
-      : (amount * udtScale) / ckbScale;
-
-    if (options.fee) {
-      const { numerator, denominator } = options.fee;
+    if (fee) {
+      const { numerator, denominator } = fee;
       convertedAmount -= (convertedAmount * numerator) / denominator;
     }
 
@@ -150,105 +146,98 @@ export class IckbController {
       system: { feeRate, tip, deposits, ckbDepositCap, exchangeRatio },
     }: Parameters<IckbController["convert"]>[0],
   ): ReturnType<IckbController["convert"]> {
-    let tx: SmartTransaction = OfflineTransaction.default();
-    const maturities: Epoch[] = [];
-    let ckbFee = ccc.Zero;
-
-    if (n > 0) {
-      if (isCkb2Udt) {
-        amount -= ckbDepositCap * BigInt(n);
-        if (amount < 0n) {
-          // Too many Deposits respectfully to the amount
-          return undefined;
-        }
-        this.logicManager.deposit(tx, n, ckbDepositCap, address.script);
-      } else {
-        const myDeposits = deposits.slice(0, n);
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        amount -= myDeposits.slice(-1)[0]!.udtCumulative;
-        if (amount < 0n) {
-          // Too many Withdrawal Requests respectfully to the amount
-          return undefined;
-        }
-        this.ownedOwnerManager.requestWithdrawal(
-          tx,
-          myDeposits,
-          address.script,
-        );
-        maturities.push(...myDeposits.map((d) => d.maturity));
-      }
-    }
-
-    if (amount > 0n) {
-      this.orderManager.mint(
-        tx,
-        address.script,
-        info,
-        isCkb2Udt ? amount : ccc.Zero,
-        isCkb2Udt ? ccc.Zero : amount,
-      );
-
-      const ratio = isCkb2Udt ? info.ckbToUdt : info.udtToCkb;
-      const convertedAmount = convert(isCkb2Udt, amount, ratio);
-      ckbFee = isCkb2Udt
-        ? amount - convert(false, convertedAmount, exchangeRatio)
-        : convert(true, amount, exchangeRatio) - convertedAmount;
-
-      maturities.push(
-        // TODO: use real world data from deposits, open orders and bot balances
-        orderMaturityEstimate(isCkb2Udt, amount, tip),
-      );
-    }
-
-    // Check that order provides enough fee to the bot for being matched
-    // feeRate is a good approximation of the bot tx size matching the order
-    if (10n * (feeRate - ckbFee) > ckbFee) {
-      return undefined;
-    }
-
-    const { receipts, withdrawalGroups, orders, udts, capacities } = userCells;
-
-    this.logicManager.completeDeposit(tx, receipts);
-    this.ownedOwnerManager.withdraw(tx, withdrawalGroups); //TODO: ignore not isReady cells in method!!!
-    for (const order of orders) {
-      this.orderManager.melt(tx, order); //TODO: allow melting multiple groups and give choices!!!
-    }
-    // const outputLen = tx.inputs.length;??
-    // Give choice that let user add this cells the way he prefer!! (Either here or at completeFeeBy)
-    this.ickbManager.addUdts(tx, udts);
-    CapacityManager.addCapacities(tx, capacities);
-    const outputLen = tx.outputs.length;
-
     try {
-      await tx.completeFeeBy(signer, feeRate);
-      // TODO: check in SmartTransaction for 64 output cells in DAO transactions
-      if (tx.outputs.length > 64) {
+      const tx = SmartTransaction.default();
+      const maturities: Epoch[] = [];
+      let ckbFee = ccc.Zero;
+
+      if (n > 0) {
+        if (isCkb2Udt) {
+          amount -= ckbDepositCap * BigInt(n);
+          if (amount < 0n) {
+            // Too many Deposits respectfully to the amount
+            return undefined;
+          }
+          this.logicManager.deposit(tx, n, ckbDepositCap, address.script);
+        } else {
+          const myDeposits = deposits.slice(0, n);
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          amount -= myDeposits.slice(-1)[0]!.udtCumulative;
+          if (amount < 0n) {
+            // Too many Withdrawal Requests respectfully to the amount
+            return undefined;
+          }
+          this.ownedOwnerManager.requestWithdrawal(
+            tx,
+            myDeposits,
+            address.script,
+          );
+          maturities.push(...myDeposits.map((d) => d.maturity));
+        }
+      }
+
+      if (amount > 0n) {
+        this.orderManager.mint(
+          tx,
+          address.script,
+          info,
+          isCkb2Udt ? amount : ccc.Zero,
+          isCkb2Udt ? ccc.Zero : amount,
+        );
+
+        const ratio = isCkb2Udt ? info.ckbToUdt : info.udtToCkb;
+        const convertedAmount = convert(isCkb2Udt, amount, ratio);
+        ckbFee = isCkb2Udt
+          ? amount - convert(false, convertedAmount, exchangeRatio)
+          : convert(true, amount, exchangeRatio) - convertedAmount;
+
+        maturities.push(
+          // TODO: use real world data from deposits, open orders and bot balances
+          orderMaturityEstimate(isCkb2Udt, amount, tip),
+        );
+      }
+
+      // Check that order provides enough fee to the bot for being matched
+      // feeRate is a good approximation of the bot tx size matching the order
+      if (10n * (feeRate - ckbFee) > ckbFee) {
         return undefined;
       }
+
+      const { receipts, withdrawalGroups, orders, udts, capacities } =
+        userCells;
+
+      this.logicManager.completeDeposit(tx, receipts);
+      this.ownedOwnerManager.withdraw(tx, withdrawalGroups, {
+        isReadyOnly: true,
+      });
+      this.orderManager.melt(tx, orders);
+      this.ickbUdtManager.addUdts(tx, udts);
+      CapacityManager.addCapacities(tx, capacities);
+
+      // Throw an error if inputs do not cover for outputs
+      await new RestrictedTransaction(tx).completeFeeBy(signer, feeRate);
+
+      // Find the maturity most distant in the future
+      const maturity = maturities.reduce((a, b) =>
+        epochCompare(a, b) === -1 ? b : a,
+      );
+
+      return {
+        tx,
+        ckbFee,
+        maturity,
+      };
+
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (_) {
       return undefined;
     }
-
-    // Revert any change cells
-    tx.outputs = tx.outputs.slice(outputLen);
-    tx = SmartTransaction.from(tx);
-
-    const maturity = maturities.reduce((a, b) =>
-      epochCompare(a, b) === -1 ? b : a,
-    );
-
-    return {
-      tx,
-      ckbFee,
-      maturity,
-    };
   }
 
   // TODO: Allow filtering which cells are fetched
   async getL1State(
     client: ccc.Client,
-    locks: ccc.ScriptLike[],
+    locks: ccc.Script[],
     options?: {
       minLockUp?: ccc.Epoch;
       maxLockUp?: ccc.Epoch;
@@ -268,18 +257,19 @@ export class IckbController {
       receipts,
       withdrawalGroups,
       orders,
+      deposits,
+      withdrawalRequests,
     ] = await Promise.all([
-      // TODO: fetch user NervosDAO cells
-      // TODO: fetch bot cells for estimating timings
       client.getFeeRate(),
-      arrayFrom(this.logicManager.findDeposits(client, opts)),
-      arrayFrom(CapacityManager.findCapacities(client, locks, opts)),
-      arrayFrom(this.ickbManager.findUdts(client, locks, opts)),
-      arrayFrom(this.logicManager.findReceipts(client, locks, opts)),
-      arrayFrom(
-        this.ownedOwnerManager.findWithdrawalGroups(client, locks, opts),
-      ),
-      this.orderManager.findOrders(client),
+      collect(this.logicManager.findDeposits(client, opts)),
+      collect(CapacityManager.findCapacities(client, locks, opts)),
+      collect(this.ickbUdtManager.findUdts(client, locks, opts)),
+      collect(this.logicManager.findReceipts(client, locks, opts)),
+      collect(this.ownedOwnerManager.findWithdrawalGroups(client, locks, opts)),
+      collect(this.orderManager.findOrders(client)),
+      collect(this.daoManager.findDeposits(client, locks, opts)),
+      collect(this.daoManager.findWithdrawalRequests(client, locks, opts)),
+      // TODO: fetch bot cells for estimating timings
     ]);
 
     // Sort depositPool by maturity
@@ -303,7 +293,7 @@ export class IckbController {
     }
 
     const exchangeRatio = ickbExchangeRatio(tip);
-    const ckbDepositCap = convert(false, ICKB_DEPOSIT_CAP, exchangeRatio); // TODO: Remove export from core
+    const ckbDepositCap = convert(false, ICKB_DEPOSIT_CAP, exchangeRatio);
 
     return {
       system: {
@@ -321,6 +311,8 @@ export class IckbController {
         receipts,
         withdrawalGroups,
         orders: userOrders,
+        deposits,
+        withdrawalRequests,
       },
     };
   }
@@ -381,6 +373,8 @@ export interface UserCells {
   receipts: ReceiptCell[];
   withdrawalGroups: WithdrawalGroups[];
   orders: OrderGroup[];
+  deposits: DaoCell[];
+  withdrawalRequests: DaoCell[];
 }
 
 export interface Conversion {
@@ -402,78 +396,4 @@ function orderMaturityEstimate(
     1n + amount / ccc.fixedPointFrom(isCkb2Udt ? 100000 : 200),
     4n * 60n,
   ]);
-}
-
-class OfflineTransaction extends SmartTransaction {
-  override clone(): OfflineTransaction {
-    const result = super.clone();
-    // Keep inputs metadata when cloning, so it will not refetch that data
-    result.inputs = [...this.inputs];
-    return new OfflineTransaction(result);
-  }
-
-  // eslint-disable-next-line @typescript-eslint/require-await
-  override async completeInputs<T>(
-    _0: never,
-    _1: never,
-    _2: never,
-    init: T,
-  ): Promise<{ addedCount: number; accumulated?: T }> {
-    // Disable completeInputs, so it will not fetch additional cells
-    return {
-      addedCount: 0,
-      accumulated: init,
-    };
-  }
-
-  // Reimplement the rest of transformations
-
-  constructor(tx: SmartTransaction) {
-    super(
-      tx.version,
-      tx.cellDeps,
-      tx.headerDeps,
-      tx.inputs,
-      tx.outputs,
-      tx.outputsData,
-      tx.witnesses,
-      tx.udtHandlers,
-      tx.headers,
-    );
-  }
-
-  static override default(): OfflineTransaction {
-    return new OfflineTransaction(super.default());
-  }
-
-  static override fromLumosSkeleton(
-    skeleton: ccc.LumosTransactionSkeletonType,
-  ): OfflineTransaction {
-    return new OfflineTransaction(super.fromLumosSkeleton(skeleton));
-  }
-
-  static override from(txLike: SmartTransactionLike): OfflineTransaction {
-    return new OfflineTransaction(SmartTransaction.from(txLike));
-  }
-}
-
-// TODO: utils.binarySearch is sync, need an async one for await the result!!!!!!!!!!!!
-async function asyncBinarySearch(
-  n: number,
-  f: (i: number) => Promise<boolean>,
-): Promise<number> {
-  // Define f(-1) == false and f(n) == true.
-  // Invariant: f(i-1) == false, f(j) == true.
-  let [i, j] = [0, n];
-  while (i < j) {
-    const h = Math.trunc((i + j) / 2);
-    // i ≤ h < j
-    if (!(await f(h))) {
-      i = h + 1; // preserves f(i-1) == false
-    } else {
-      j = h; // preserves f(j) == true
-    }
-  }
-  // i == j, f(i-1) == false, and f(j) (= f(i)) == true  =>  answer is i.
-  return i;
 }
